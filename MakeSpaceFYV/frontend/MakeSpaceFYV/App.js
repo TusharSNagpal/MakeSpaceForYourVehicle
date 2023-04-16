7cdb61248c5c2aa4e16fd54ddd90494421438ee4
TusharSNagpal <nagpal8795@gmail.com>
2023-04-16 23:19:48 +0530

    CRUD-properties

--- MakeSpaceFYV/frontend/MakeSpaceFYV/App.js
+++ MakeSpaceFYV/frontend/MakeSpaceFYV/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>MAKE SPACE FOR YOUR VEHICLE UNDER DEVELOPMENT!</Text>
+      <Text>MAKE SPACE FOR YOUR VEHICLE UNDER DEVELOPMENT!!</Text>
       <StatusBar style="auto" />
     </View>
   );
