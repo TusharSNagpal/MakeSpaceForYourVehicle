9323d1f43bef336ff02608cd8e23afabe6f948b3
TusharSNagpal <nagpal8795@gmail.com>
2023-04-16 18:53:27 +0530

    CRUD-properties

--- MakeSpaceFYV/backend/config/db.js
+++ MakeSpaceFYV/backend/config/db.js
@@ -11,12 +11,11 @@ const mongoDB = async() => {
 
     try{
         conn = await mongoose.connect(uri)
-        console.log(`MongoDB database connection established successfully: ${conn.connection.host}`.cyan.underline);
+        console.log(`MongoDB database connection established successfully : ${conn.connection.host}`.cyan.underline);
     } catch (error){
         console.log(error);
         process.exit(1);
     }
-
 }
 
 module.exports = mongoDB; //we don't want to call it here, we want to export it so don't use () method here..!
