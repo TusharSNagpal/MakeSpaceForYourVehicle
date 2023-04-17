0b075c2ecadefa3f2989cd5766379838fd17c9cb
Gaurav-Tilokani <gaurav.180410107121@gmail.com>
2023-04-17 12:09:05 +0530

    customer added

--- MakeSpaceFYV/backend/routes/ownerRoutes.js
+++ MakeSpaceFYV/backend/routes/ownerRoutes.js
@@ -4,6 +4,6 @@ const {registerOwner, loginOwner, getOwner} = require('../controllers/ownerContr
 
 router.post('/', registerOwner)
 router.post('/login', loginOwner)
-router.get('/get', getOwner)
+router.get('/getOwner', getOwner)
 
 module.exports = router
