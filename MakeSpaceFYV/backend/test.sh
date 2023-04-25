b1b4274426d7052d73dadadd9c637864aff413a8
Tushar Nagpal <63884440+TusharSNagpal@users.noreply.github.com>
2023-04-25 16:45:26 +0530

    Update test.sh

--- MakeSpaceFYV/backend/test.sh
+++ MakeSpaceFYV/backend/test.sh
@@ -2,7 +2,7 @@
 RHOST=8.8.8.8  
 MYIP=$(ip route get $RHOST | awk '{ print $7; exit }')
 kubectl port-forward --address localhost,$MYIP service/msfyv-backend-service 5001:5001 & \
-kubectl port-forward --address localhost,$MYIP service/msfyv-frontend-service 19000:19000 & \
+# kubectl port-forward --address localhost,$MYIP service/msfyv-frontend-service 19000:19000 & \
 
 echo "Port Forwarding Successful"
 wait
