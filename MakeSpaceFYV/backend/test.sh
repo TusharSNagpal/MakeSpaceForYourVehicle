da95da01d09519a7c335e6ed8608753a101358f6
Tushar Nagpal <63884440+TusharSNagpal@users.noreply.github.com>
2023-04-25 18:20:22 +0530

    Delete test.sh

--- MakeSpaceFYV/backend/test.sh
+++
@@ -1,8 +0,0 @@
-# Push commands in the background, when the script exits, the commands will exit too
-RHOST=8.8.8.8  
-MYIP=$(ip route get $RHOST | awk '{ print $7; exit }')
-kubectl port-forward --address localhost,$MYIP service/msfyv-backend-service 5001:5001 & \
-# kubectl port-forward --address localhost,$MYIP service/msfyv-frontend-service 19000:19000 & \
-
-echo "Port Forwarding Successful"
-wait
